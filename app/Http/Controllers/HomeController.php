6a20ffacd4f600d6bae386fce5c0ffdfaefa7d7d
Karunagaran Sheroll <sherollk02@gmail.com>
2024-03-24 16:58:16 +0530

    Caching

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -13,9 +14,17 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('home',[
-            'featuredPosts' => Post::published()->featured()->with('categories')->latest('published_at')->take(3)->get(),
-            'latestPosts' => Post::published()->with('categories')->latest('published_at')->take(6)->get(),
+        $featuredPosts = Cache::remember('featuredPosts', now()->addDay(), function () {
+            return Post::published()->featured()->with('categories')->latest('published_at')->take(3)->get();
+        });
+
+        $latestPosts = Cache::remember('latestPosts', now()->addDay(), function () {
+            return Post::published()->with('categories')->latest('published_at')->take(6)->get();
+        });
+
+        return view('home', [
+            'featuredPosts' => $featuredPosts,
+            'latestPosts' => $latestPosts
         ]);
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $categories = Cache::remember('categories', now()->addDays(3), function () {
+            return Category::whereHas('posts', function ($query) {
+                $query->published();
+            })->take(10)->get();
+        });
+
         return view('posts.index',
             [
-                'categories' => Category::all(),
+                'categories' => $categories,
             ]
         );
         
