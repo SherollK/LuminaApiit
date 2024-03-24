1269aac8ce1cbb644a0ba2cb6506b72bdb3aad40
Karunagaran Sheroll <sherollk02@gmail.com>
2024-03-24 22:57:24 +0530

    updated the nav bar color

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
 @props(['active', 'navigate'])
 
 @php
-    $classes = $active ?? false ? 'inline-flex items-center hover:text-yellow-900 text-sm text-yellow-500' : 'inline-flex items-center hover:text-yellow-900 text-sm text-gray-500';
+    $classes = $active ?? false ? 'inline-flex items-center hover:text-teal-900 text-sm text-teal-500' : 'inline-flex items-center hover:text-teal-900 text-sm text-gray-500';
 @endphp
 
 <a {{ $navigate ?? true ? 'wire:navigate' : '' }} {{ $attributes->merge(['class' => $classes]) }}>
