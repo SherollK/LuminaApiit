387441b4878af03da2786ebd7a62ea38c9f22f2f
Karunagaran Sheroll <sherollk02@gmail.com>
2024-03-24 23:14:13 +0530

    Displaying all the categories

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,21 +3,14 @@
 namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $categories = Cache::remember('categories', now()->addDays(3), function () {
-            return Category::whereHas('posts', function ($query) {
-                $query->published();
-            })->take(10)->get();
-        });
-
         return view('posts.index',
             [
-                'categories' => $categories,
+                'categories' => Category::all(),
             ]
         );
         
